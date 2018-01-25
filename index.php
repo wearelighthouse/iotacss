c7c586de5a24589b366c473c55b649cc51ab52ef
Russell Bishop <hey@russellbishop.co.uk>
2018-01-25 09:24:58 +0000

    Move to assets/

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Map Utilities, Bro</title>
+    <link rel="stylesheet" type="text/css" href="/dist/css/main.css">
+</head>
+<body>
+
+<div class="o-container">
+
+</div>
+
+</body>
+</html>
